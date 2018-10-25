bee7323f09dff9813339891e47964b4c2c06568e
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2018-10-26 05:01:02 +0800

    Update Shader.cc

--- sources/Shader.cc
+++ sources/Shader.cc
@@ -12,7 +12,36 @@ void Shader::UnBind() const;
 void Shader::SetUniform4f(const std::string& name, float f0, float f1, float f2, float f3);
 unsigned int Shader::GetUniformLocation(const std::string& name);
 
+void ParseShader(const std::string& filepath, std::string& VertexShader, std::string& FragmentShader)
+{
+	std::ifstream stream(filepath);
 
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	ShaderType type = ShaderType::NONE;
+
+	std::string line;
+	std::stringstream s[2];
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else
+		{
+			s[int(type)] << line << "\n";
+		}
+	}
+
+	VertexShader = s[0].str();
+	FragmentShader = s[1].str();
+}
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
 	unsigned int id = glCreateShader(type);
