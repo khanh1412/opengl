a7bcffb65122ff9b079b0198928f49efaa777143
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-25 02:48:29 -0700

    modified:   main.cc

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
+#include<iostream>
 
 int main(void)
 {
@@ -9,7 +10,6 @@ int main(void)
 	
 	if (!glfwInit())
 		return -1;
-	glewInit();
 
 	window = glfwCreateWindow(640, 480, "Hello World", 0x0, 0x0);
 
@@ -19,10 +19,39 @@ int main(void)
 		return -1;
 	}
 
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+		std::cout<< "Error!"<<std::endl;
+
+	std::cout<<glGetString(GL_VERSION)<<std::endl;
+
+	//prepare buffer
+
+	float position[6] = 
+	{
+		-0.5, -0.5,
+		 0.0,  0.5,
+		 0.5, -0.5
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), &(position[0]), GL_STATIC_DRAW);
+
+	//end prepare buffer
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//render
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+
+		//end render
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
