930cd1953d6026b60731f5e936d98c7e69055432
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2018-10-26 05:00:28 +0800

    Update Shader.cc

--- sources/Shader.cc
+++ sources/Shader.cc
@@ -13,7 +13,50 @@ void Shader::SetUniform4f(const std::string& name, float f0, float f1, float f2,
 unsigned int Shader::GetUniformLocation(const std::string& name);
 
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char *src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+	
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char *message = new char[length];
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout<<"Failed to compile"<<
+			(type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+			<<" shader!"<<std::endl;
+		std::cout<<message<<std::endl;
+		glDeleteShader(id);
+		delete message;
+		return 0;
+	}
+	return id;
+}
 
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program =  glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 
 
