18486e6b2f0cb2fe8832cb80d8281501fe4e9796
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-25 15:38:45 -0700

    modified:   make.bat

--- make.bat
+++ make.bat
@@ -1,1 +1,7 @@
-g++ -std=c++11 -o run.exe main.cc sources\Renderer.cc sources\VertexBuffer.cc sources\IndexBuffer.cc sources\VertexArray.cc sources\VertexBufferLayout.cc sources\Shader.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -c -fPIC -o objects\Renderer.o sources\Renderer.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -c -fPIC -o objects\VertexArray.o sources\VertexArray.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -c -fPIC -o objects\VertexBuffer.o sources\VertexBuffer.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -c -fPIC -o objects\VertexBufferLayout.o sources\VertexBufferLayout.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -c -fPIC -o objects\IndexBuffer.o sources\IndexBuffer.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -c -fPIC -o objects\Shader.o sources\Shader.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -o libRenderer.dll objects\Renderer.o objects\VertexArray.o objects\VertexBuffer.o objects\VertexBufferLayout.o objects\IndexBuffer.o objects\Shader.o .\glew32.dll .\glfw3.dll -I.\include -lopengl32
