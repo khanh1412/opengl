f731111d410df70073c0fc53c26831f9fa513443
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-25 16:05:07 -0700

    modified:   include/Renderer.h 	modified:   include/VertexBufferLayout.h 	new file:   libRenderer.dll 	modified:   make.bat 	deleted:    sources/VertexBufferLayout.cc

--- include/Renderer.h
+++ include/Renderer.h
@@ -13,7 +13,7 @@
 class Renderer
 {
 	public:
-//		void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+		void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
 };
 
 
--- include/VertexBufferLayout.h
+++ include/VertexBufferLayout.h
@@ -8,7 +8,16 @@ struct VertexBufferElement
 	unsigned int count;
 	unsigned char normalized;
 
-	static unsigned int GetSizeOfType(unsigned int type);
+	static unsigned int GetSizeOfType(unsigned int type)
+	{
+		switch (type)
+		{
+			case GL_FLOAT: 		return 4;
+			case GL_UNSIGNED_INT: 	return 4;
+			case GL_UNSIGNED_BYTE: 	return 1;
+		}
+		return 0;
+	}
 };
 class VertexBufferLayout
 {
@@ -19,37 +28,24 @@ class VertexBufferLayout
 		VertexBufferLayout()
 			: m_Stride(0) {}
 
-		void Push_float(unsigned int count);
-		void Push_unsigned_int(unsigned int count);
-		void Push_unsigned_char(unsigned int count);
+		void Push_float(unsigned int count)
+		{
+			m_Elements.push_back( {GL_FLOAT, count, GL_FALSE} );
+			m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT) * count;
+		}
+		void Push_unsigned_int(unsigned int count)
+		{
+			m_Elements.push_back( {GL_UNSIGNED_INT, count, GL_FALSE} );
+			m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT) * count;
+		}
+		void Push_unsigned_char(unsigned int count)
+		{
+			m_Elements.push_back( {GL_UNSIGNED_BYTE, count, GL_TRUE} );
+			m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE) * count;
+		}
 
 
 		inline const std::vector<VertexBufferElement> GetElements() const {return m_Elements;}
 		inline unsigned int GetStride() const {return m_Stride;}
 };
-void VertexBufferLayout::Push_float(unsigned int count)
-{
-	m_Elements.push_back( {GL_FLOAT, count, GL_FALSE} );
-	m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT) * count;
-}
-void VertexBufferLayout::Push_unsigned_int(unsigned int count)
-{
-	m_Elements.push_back( {GL_UNSIGNED_INT, count, GL_FALSE} );
-	m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT) * count;
-}
-void VertexBufferLayout::Push_unsigned_char(unsigned int count)
-{
-	m_Elements.push_back( {GL_UNSIGNED_BYTE, count, GL_TRUE} );
-	m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE) * count;
-}
-unsigned int VertexBufferElement::GetSizeOfType(unsigned int type);
-{
-	switch (type)
-	{
-		case GL_FLOAT: 		return 4;
-		case GL_UNSIGNED_INT: 	return 4;
-		case GL_UNSIGNED_BYTE: 	return 1;
-	}
-	return 0;
-}
 #endif
