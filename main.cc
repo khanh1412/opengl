31816f39b7d11b6320d88d9f9c5c2413c1bfe8fd
#NGUYEN NGOC KHANH# <C170030@student.main.ntu.edu.sg>
2018-10-25 15:57:12 +0800

    t	new file:   include/GL/eglew.h 	new file:   include/GL/glew.h 	new file:   include/GL/glxew.h 	new file:   include/GL/wglew.h 	new file:   lib/glew32.lib 	new file:   lib/glew32s.lib 	renamed:    glfw3.dll -> lib/glfw3.dll 	renamed:    libglfw3.a -> lib/libglfw3.a 	renamed:    libglfw3dll.a -> lib/libglfw3dll.a 	modified:   main.cc 	modified:   make.bat

--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 #include <GLFW/glfw3.h>
-
+#include<iostream>
 int main(void)
 {
 	GLFWwindow* window;
@@ -19,11 +19,46 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+
+	std::cout<<glGetString(GL_VERSION)<<std::endl;
+
+
+	float position[6] =
+	{
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f	
+	};
+
+	unsigned int buffer;
+
+	glGenBufffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), position, GL_STATIC_DRAW);
+
+
+
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 	/* Render here */
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f( 0.0f,  0.5f);
+	glVertex2f( 0.5f, -0.5f);
+
+
+	glEnd();
+
+/*
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+*/
+
+
 
 	/* Swap front and back buffers */
 	glfwSwapBuffers(window);
--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-g++ -std=c++11 -o run.exe main.cc glfw3.dll -I.\include -lopengl32 
+g++ -std=c++11 -o run.exe main.cc glfw3.dll glew32.lib -I.\include -lopengl32
