c2ede5a049718dc3e6849552215690c588abfd77
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-25 04:59:02 -0700

    modified:   main.cc

--- main.cc
+++ main.cc
@@ -92,7 +92,7 @@ int main(void)
 		"\n"
 		"layout(location = 0) in vec4 position;\n"
 		"\n"
-		"void main()"
+		"void main()\n"
 		"{\n"
 		"	gl_Position = position;\n"
 		"}\n";
@@ -103,7 +103,7 @@ int main(void)
 		"\n"
 		"layout(location = 0) out vec4 color;\n"
 		"\n"
-		"void main()"
+		"void main()\n"
 		"{\n"
 		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
 		"}\n";
@@ -129,6 +129,8 @@ int main(void)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteShader(shader);
 
 	glfwTerminate();
 	return 0;
