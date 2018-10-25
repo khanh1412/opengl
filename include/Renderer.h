b95dee2c914a1e15809aedbf12421e016c9c8a2a
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-25 15:08:28 -0700

    modified:   include/Renderer.h

--- include/Renderer.h
+++ include/Renderer.h
@@ -2,7 +2,20 @@
 #define _RENDERER_H_
 
 #include<GL/glew.h>
+#include"VertexArray.h"
+#include"IndexArray.h"
+#include"Shader.h"
+
 
 #define ASSERT(x) if (!(x)) __builtin_trap()
+
+class Renderer
+{
+	public:
+		void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+}
+
+
+
 
 #endif
