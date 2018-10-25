3444e9bdd6085e9c4ef069a6164aba97e69bc640
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-25 16:27:11 -0700

    modified:   include/VertexBufferLayout.h 	modified:   libRenderer.dll 	modified:   make.bat 	new file:   sources/VertexBufferLayout.cc

--- include/VertexBufferLayout.h
+++ include/VertexBufferLayout.h
@@ -8,16 +8,7 @@ struct VertexBufferElement
 	unsigned int count;
 	unsigned char normalized;
 
-	static unsigned int GetSizeOfType(unsigned int type)
-	{
-		switch (type)
-		{
-			case GL_FLOAT: 		return 4;
-			case GL_UNSIGNED_INT: 	return 4;
-			case GL_UNSIGNED_BYTE: 	return 1;
-		}
-		return 0;
-	}
+	static unsigned int GetSizeOfType(unsigned int type);
 };
 class VertexBufferLayout
 {
@@ -28,21 +19,9 @@ class VertexBufferLayout
 		VertexBufferLayout()
 			: m_Stride(0) {}
 
-		void Push_float(unsigned int count)
-		{
-			m_Elements.push_back( {GL_FLOAT, count, GL_FALSE} );
-			m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT) * count;
-		}
-		void Push_unsigned_int(unsigned int count)
-		{
-			m_Elements.push_back( {GL_UNSIGNED_INT, count, GL_FALSE} );
-			m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT) * count;
-		}
-		void Push_unsigned_char(unsigned int count)
-		{
-			m_Elements.push_back( {GL_UNSIGNED_BYTE, count, GL_TRUE} );
-			m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE) * count;
-		}
+		void Push_float(unsigned int count);
+		void Push_unsigned_int(unsigned int count);
+		void Push_unsigned_char(unsigned int count);
 
 
 		inline const std::vector<VertexBufferElement> GetElements() const {return m_Elements;}
--- make.bat
+++ make.bat
@@ -1,7 +1,8 @@
-g++ -std=c++11 -c -fPIC -o objects\Renderer.o sources\Renderer.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
-g++ -std=c++11 -c -fPIC -o objects\VertexArray.o sources\VertexArray.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
-g++ -std=c++11 -c -fPIC -o objects\VertexBuffer.o sources\VertexBuffer.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
-g++ -std=c++11 -c -fPIC -o objects\IndexBuffer.o sources\IndexBuffer.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
-g++ -std=c++11 -c -fPIC -o objects\Shader.o sources\Shader.cc .\glew32.dll .\glfw3.dll -I.\include -lopengl32
-g++ -std=c++11 -shared -o libRenderer.dll objects\Renderer.o objects\VertexArray.o objects\VertexBuffer.o objects\IndexBuffer.o objects\Shader.o .\glew32.dll .\glfw3.dll -I.\include -lopengl32
-g++ -std=c++11 -o run.exe main.cc .\libRenderer.dll .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -I.\include -c -fPIC -o objects\Renderer.o sources\Renderer.cc 		
+g++ -std=c++11 -I.\include -c -fPIC -o objects\VertexArray.o sources\VertexArray.cc 	
+g++ -std=c++11 -I.\include -c -fPIC -o objects\VertexBuffer.o sources\VertexBuffer.cc 	
+g++ -std=c++11 -I.\include -c -fPIC -o objects\IndexBuffer.o sources\IndexBuffer.cc 	
+g++ -std=c++11 -I.\include -c -fPIC -o objects\Shader.o sources\Shader.cc 			
+g++ -std=c++11 -I.\include -c -fPIC -o objects\VertexBufferLayout.o sources\VertexBufferLayout.cc
+g++ -std=c++11 -I.\include -shared -o libRenderer.dll objects\Renderer.o objects\VertexArray.o objects\VertexBuffer.o objects\IndexBuffer.o objects\Shader.o objects\VertexBufferLayout.o .\glew32.dll .\glfw3.dll -I.\include -lopengl32
+g++ -std=c++11 -I.\include -o run.exe main.cc .\libRenderer.dll .\glew32.dll .\glfw3.dll -lopengl32
