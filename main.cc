f0215ca13998d2c9402d5e108eabae96a9805426
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-25 09:14:14 -0700

    new file:   glew32.dll 	modified:   main.cc 	modified:   make.bat

--- main.cc
+++ main.cc
@@ -1,70 +1,30 @@
-#include <GLFW/glfw3.h>
-#include<iostream>
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+
 int main(void)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
+	//intialize the library
+	
 	if (!glfwInit())
 		return -1;
+	glewInit();
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Hello World", 0x0, 0x0);
+
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
-
-	std::cout<<glGetString(GL_VERSION)<<std::endl;
-
-/*
-	float position[6] =
-	{
-		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f	
-	};
-
-	unsigned int buffer;
-
-	glGenBufffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), position, GL_STATIC_DRAW);
-*/
-
-
-
-	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
-	/* Render here */
-	glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_TRIANGLES);
-	glVertex2f(-0.5f, -0.5f);
-	glVertex2f( 0.0f,  0.5f);
-	glVertex2f( 0.5f, -0.5f);
-
-
-	glEnd();
-
-/*
-
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-*/
-
-
-
-	/* Swap front and back buffers */
-	glfwSwapBuffers(window);
-
-	/* Poll for and process events */
-	glfwPollEvents();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
 	glfwTerminate();
