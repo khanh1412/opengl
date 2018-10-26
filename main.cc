34919036a706e9d2ebc22399f0d1b69d065dffd2
Nguyen Ngoc Khanh <31823828+khanh1412@users.noreply.github.com>
2018-10-26 08:50:15 +0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -15,7 +15,31 @@
 #include<cmath>
 
 
+void renderScene()
+{
+	glEnable(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		GL_RGB,
+		WIDTH,
+		HEIGHT,
+		0,
+		GL_RGB,
+		GL_UNSIGNED_BYTE,
+		&texture[0][0][0]		
+	);
+	glBegin(GL_QUADS);
+		glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0, -1.0);
+		glTexCoord2f(1.0f, 0.0f); glVertex2f( 1.0, -1.0);
+		glTexCoord2f(1.0f, 1.0f); glVertex2f( 1.0,  1.0);
+		glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0,  1.0);
+	glEnd();
 
+	glFlush();
+	glutSwapBuffers();
+}
 
 
 int main(void)
