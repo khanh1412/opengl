3e82c16a924f6acc783cec740f069e24e6fc0345
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-30 21:40:03 +0800

    modified:   make.bat

--- make.bat
+++ make.bat
@@ -9,5 +9,6 @@ g++ -std=c++11 -I.\include -c -fPIC -o objects\Texture.o sources\Texture.cc
 g++ -std=c++11 -I.\include\vendor -c -fPIC -o objects\stb_image.o sources\vendor\stb_image.cc
 g++ -std=c++11 -I.\include -shared -o libRenderer.dll objects\Renderer.o objects\VertexArray.o objects\VertexBuffer.o objects\IndexBuffer.o objects\Shader.o objects\VertexBufferLayout.o objects\Texture.o objects\stb_image.o .\glew32.dll .\glfw3.dll -I.\include -lopengl32
 g++ -std=c++11 -I.\include -o run.exe main.cc .\libRenderer.dll .\glew32.dll .\glfw3.dll -lopengl32
+
 del objects\*.o
 rmdir objects
