6cb6c9cdcf4355359660582ac606e05cc5e588d7
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2018-10-30 21:27:33 +0800

    modified:   libRenderer.dll 	modified:   make.bat

--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
+mkdir objects
 g++ -std=c++11 -I.\include -c -fPIC -o objects\Renderer.o sources\Renderer.cc 		
 g++ -std=c++11 -I.\include -c -fPIC -o objects\VertexArray.o sources\VertexArray.cc 	
 g++ -std=c++11 -I.\include -c -fPIC -o objects\VertexBuffer.o sources\VertexBuffer.cc 	
@@ -8,3 +9,5 @@ g++ -std=c++11 -I.\include -c -fPIC -o objects\Texture.o sources\Texture.cc
 g++ -std=c++11 -I.\include\vendor -c -fPIC -o objects\stb_image.o sources\vendor\stb_image.cc
 g++ -std=c++11 -I.\include -shared -o libRenderer.dll objects\Renderer.o objects\VertexArray.o objects\VertexBuffer.o objects\IndexBuffer.o objects\Shader.o objects\VertexBufferLayout.o objects\Texture.o objects\stb_image.o .\glew32.dll .\glfw3.dll -I.\include -lopengl32
 g++ -std=c++11 -I.\include -o run.exe main.cc .\libRenderer.dll .\glew32.dll .\glfw3.dll -lopengl32
+del objects\*.o
+rmdir objects
