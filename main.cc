c5a1f04f8fadb4d08b7a00901717ac5c64e28ef5
Nguyen Ngoc Khanh <khanh@localhost.localdomain>
2018-11-13 23:50:05 +0800

    modified:   main.cc

--- main.cc
+++ main.cc
@@ -15,19 +15,19 @@ int main()
 	Engine E(640, 480, "world");
 
 
-	Object *S = new Sphere(0.5, 0.5, 0.5);
-	Object *R = new Rectangle(1, 0, 1, -1, 0, 1, 1, 1, 0);
-	Object *C = new Cylinder(1, -1, -1, -1, 1, 1, 0.3);
-	R->setTexture("./resources/textures/a.png");
+	Object *S = new Sphere(0.0, 0.0, 0.0, 0.1);
+	//Object *R = new Rectangle(1, 0, 1, -1, 0, 1, 1, 1, 0);
+	Object *C = new Cylinder(0, 1, 0.1, 0, 0, 0, 1);
+	//R->setTexture("./resources/textures/a.png");
 	while (!E.isClosed())
 	{
 		E.clear();
-		E.setCam(0, 0, 2);
+		E.setCam(0, 2, 2);
 		E.setCenter(0, 0, 0);
 		E.setUp(0,1,0);
 		E.setPov(2.0);
 
-		E.draw(R);
+	//	E.draw(R);
 		E.draw(S);
 		E.draw(C);
 		E.swapBuffers();
